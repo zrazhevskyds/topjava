82cea8e6b94f4e2d39704e823b8676675055d065
Denis <zrazhevskyds@hotmail.com>
2019-12-27 16:16:49 +0200

    Prepare to HW0

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -1,11 +1,14 @@
 package ru.javawebinar.topjava;
 
 /**
- * @see <a href="http://topjava.herokuapp.com">Demo application</a>
- * @see <a href="https://github.com/JavaOPs/topjava">Initial project</a>
+ * User: gkislin
+ * Date: 05.08.2015
+ *
+ * @link http://caloriesmng.herokuapp.com/
+ * @link https://github.com/JavaOPs/topjava
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.format("Hello TopJava Enterprise!");
+        System.out.format("Hello Topjava Enterprise!");
     }
 }
